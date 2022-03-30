bab34071674651485ddb7730f28761baf61bbcec
Azazel-h <arseniy2003bulgakov@gmail.com>
2022-03-30 21:59:46 +0300

    Basics added

--- Interferometer/urls.py
+++ Interferometer/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-import visualization
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('visualization.urls'))
+    path('', include('visualization.urls')),
+    path('', include('accounts.urls')),
 ]
