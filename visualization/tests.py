e258da57a82583cf6379af3f404367e83bb33cc1
Azazel-h <arseniy2003bulgakov@gmail.com>
2022-04-01 22:53:42 +0300

    CI/CD + one test

--- visualization/tests.py
+++ visualization/tests.py
@@ -1,3 +1,13 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
 
-# Create your tests here.
+class TestPage(TestCase):
+   def setUp(self):
+       self.client = Client()
+
+   def test_index_page(self):
+       url = reverse('index')
+       response = self.client.get(url)
+       self.assertEqual(response.status_code, 200)
+       self.assertTemplateUsed(response, 'index.html')
+
