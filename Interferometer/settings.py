65baf966c19539a47f4c7fd74dda3a132eb7efca
Azazel-h <arseniy2003bulgakov@gmail.com>
2022-04-02 08:30:56 +0300

    More comfortable UI

--- Interferometer/settings.py
+++ Interferometer/settings.py
@@ -39,7 +39,10 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'accounts',
     'visualization',
+    'crispy_forms'
 ]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
