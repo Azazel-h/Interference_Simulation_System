6250eb63672061c778e4958731adb3e0d963da8d
Bulgakov Arseniy <1446-bas21u810@users.noreply.git.iu7.bmstu.ru>
2022-04-09 15:02:52 +0300

    Min / Max values restrictions

--- visualization/forms.py
+++ visualization/forms.py
@@ -1,11 +1,11 @@
 from django import forms
 
 class GraphForm(forms.Form):
-    wave_length = forms.FloatField(label='Длинна волны [nm]',min_value=0.1)
-    glasses_distance = forms.FloatField(label='Расстояние между стеклами [mm]')
-    focal_distance = forms.FloatField(label='Фокусное расстояние линзы [mm]')
-    stroke_difference = forms.FloatField(label='Разница хода [nm]')
-    reflectivity = forms.FloatField(label='Коэффициент отражения [--]')
-    picture_size = forms.FloatField(label='Размер рисунка [mm]')
+    wave_length = forms.FloatField(label='Длинна волны [nm]', min_value=0.1)
+    glasses_distance = forms.FloatField(label='Расстояние между стеклами [mm]', min_value=0)
+    focal_distance = forms.FloatField(label='Фокусное расстояние линзы [mm]', min_value=0)
+    stroke_difference = forms.FloatField(label='Разница хода [nm]', min_value=0)
+    reflectivity = forms.FloatField(label='Коэффициент отражения [--]', min_value=0, max_value=1)
+    picture_size = forms.FloatField(label='Размер рисунка [mm]', min_value=0)
     N = forms.IntegerField(label='Разрешение', min_value=1)
-    NMEDIUM = forms.FloatField(label='NMEDIUM')
+    NMEDIUM = forms.FloatField(label='Коэфицент (NMEDIUM)', min_value=0)
