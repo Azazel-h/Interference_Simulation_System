670693c5e7c7a2dd9d96cd2ceba9259946eeba22
Bulgakov Arseniy <1446-bas21u810@users.noreply.git.iu7.bmstu.ru>
2022-04-16 17:24:10 +0300

    Laser color

--- visualization/forms.py
+++ visualization/forms.py
@@ -1,11 +1,12 @@
 from django import forms
 
 class GraphForm(forms.Form):
-    wave_length = forms.FloatField(label='Длинна волны [нм]', min_value=0.1)
+    laser_color = forms.ChoiceField(label='Цвет лазера', choices=(('g', 'Зеленый'), ('r', 'Красный')))
+    wave_length = forms.FloatField(label='Длинна волны [нм]', min_value=380, max_value=780)
     glasses_distance = forms.FloatField(label='Расстояние между стеклами [мм]', min_value=0)
     focal_distance = forms.FloatField(label='Фокусное расстояние линзы [мм]', min_value=0)
     stroke_difference = forms.FloatField(label='Разница хода [нм]', min_value=0)
-    reflectivity = forms.FloatField(label='Коэффициент отражения [--]', min_value=0, max_value=0.999)
+    reflectivity = forms.FloatField(label='Коэффициент отражения [--]', min_value=0, max_value=0.9999)
     picture_size = forms.FloatField(label='Размер рисунка [мм]', min_value=0)
     N = forms.IntegerField(label='Разрешение', min_value=1)
-    NMEDIUM = forms.FloatField(label='Коэфицент умножения', min_value=0)
+    NMEDIUM = forms.FloatField(label='Коэфицент отдаления', min_value=1)
--- visualization/views.py
+++ visualization/views.py
@@ -16,6 +16,7 @@ def index_page(request) -> render:
             wave_length = form.cleaned_data['wave_length'] * nm
             stroke_difference = form.cleaned_data['stroke_difference'] * nm
             reflectivity = form.cleaned_data['reflectivity']
+            color = form.cleaned_data['laser_color']
 
             N = form.cleaned_data['N']
             NMEDIUM = form.cleaned_data['NMEDIUM']
@@ -23,15 +24,15 @@ def index_page(request) -> render:
             k = 2 * math.pi / wave_length
 
             graph = get_graph(stroke_difference, NMEDIUM, wave_length, picture_size,
-                              N, glasses_distance, reflectivity, focal_distance, k)
+                              N, glasses_distance, reflectivity, focal_distance, k, color)
             context['graph'] = graph
     else:
         form = GraphForm()
     context['form'] = form
-
     return render(request, 'index.html', context=context)
 
-def get_graph(stroke_difference, NMEDIUM, wave_length, picture_size, N, glasses_distance, reflectivity, focal_distance, k):
+def get_graph(stroke_difference, NMEDIUM, wave_length, picture_size, N,
+              glasses_distance, reflectivity, focal_distance, k, laser_color):
     second_k = 2 * math.pi / (wave_length + stroke_difference)
     fineness = 4.0 * reflectivity / (1.0 - reflectivity)
 
@@ -56,7 +57,14 @@ def get_graph(stroke_difference, NMEDIUM, wave_length, picture_size, N, glasses_
             delta = second_k * NMEDIUM * glasses_distance * math.cos(theta)
             I[i][j] = (Inten + 0.5 / (1 + fineness * math.pow(math.sin(delta), 2)))
 
-    fig = px.imshow(I)
-    graph = fig.to_html(full_html=False)
+    # color_scale = [(0, 'purple'), (0.13, 'blue'), (0.23, 'aqua'), (0.35, 'lime'),
+    #              (0.55, 'yellow'), (0.7, 'red'), (0.9, 'red'), (1, 'maroon')]
+    config = {'scrollZoom': True, 'toImageButtonOptions': { 'height': None, 'width': None}}
+    if laser_color == 'g':
+        fig = px.imshow(I, color_continuous_scale=[(0, '#013b00'), (1, 'lime')])
+    else:
+        fig = px.imshow(I, color_continuous_scale=[(0, '#3b0000'), (1, 'red')])
 
+    print(px.colors.sequential.Inferno)
+    graph = fig.to_html(full_html=False, config=config)
     return graph
