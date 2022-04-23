2da658358adc8ec3ec83f76e446785e96fa3507c
DionisPalpatin <77577723+DionisPalpatin@users.noreply.github.com>
2022-04-23 09:49:07 +0300

    Update forms.py

--- visualization/forms.py
+++ visualization/forms.py
@@ -6,5 +6,6 @@ class GraphForm(forms.Form):
     focal_distance = forms.FloatField(label='Фокусное расстояние линзы [мм]', min_value=0)
     stroke_difference = forms.FloatField(label='Разница хода [нм]', min_value=0)
     reflectivity = forms.FloatField(label='Коэффициент отражения [--]', min_value=0, max_value=0.9999)
+    refractive_index = forms.FloatField(label='Коэффициент преломления', min_value=1)
     picture_size = forms.FloatField(label='Размер рисунка [мм]', min_value=0)
     N = forms.IntegerField(label='Разрешение', min_value=1)
