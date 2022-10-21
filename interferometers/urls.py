894da6f55d1b12a330519d1ad3bb18dbd8271099
Sersoid <36604673+Sersoid@users.noreply.github.com>
2022-10-21 15:28:32 +0300

    Rename apps and some front updates

--- interferometers/urls.py
+++ interferometers/urls.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from . import views
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('visualization.urls')),
-    path('michelson/', include('michelson.urls')),
+    path('', views.home_page),
     path('accounts/', include('accounts.urls')),
+    path('admin/', admin.site.urls),
+    path('fabry-perot/', include('fabry_perot.urls')),
+    path('michelson/', include('michelson.urls')),
 ]
