0153802f79e1c9c34b5b96d5dfcab475c5ff100a
Sers <serspgl@gmail.com>
2022-11-11 22:53:55 +0300

    Fix autoFill.js

--- static/js/autoFill.js
+++ static/js/autoFill.js
@@ -1,11 +1,13 @@
-function autoFill(num) {
-    document.getElementById("id_wave_length").value = document.getElementById("wave-length-" + num).innerText;
-    document.getElementById("id_glasses_distance").value = document.getElementById("glasses-dist-" + num).innerText;
-    document.getElementById("id_focal_distance").value = document.getElementById("focal-distance-" + num).innerText;
-    document.getElementById("id_stroke_difference").value = document.getElementById("stroke-difference-" + num).innerText;
-    document.getElementById("id_reflectivity").value = document.getElementById("reflectivity-" + num).innerText;
-    document.getElementById("id_refractive_index").value = document.getElementById("refractive-index-" + num).innerText;
-    document.getElementById("id_picture_size").value = document.getElementById("picture-size-" + num).innerText;
-    document.getElementById("id_incident_light_intensity").value = document.getElementById("incident-light-intensive-" + num).innerText;
-    document.getElementById("id_N").value = document.getElementById("n-" + num).innerText;
+function autoFill(num, source) {
+    const source_tr = document.getElementById(source).children[1].children[num - 1];
+
+    document.getElementById("id_wave_length").value = source_tr.children["wave-length-" + num].innerText;
+    document.getElementById("id_glasses_distance").value = source_tr.children["glasses-dist-" + num].innerText;
+    document.getElementById("id_focal_distance").value = source_tr.children["focal-distance-" + num].innerText;
+    document.getElementById("id_stroke_difference").value = source_tr.children["stroke-difference-" + num].innerText;
+    document.getElementById("id_reflectivity").value = source_tr.children["reflectivity-" + num].innerText;
+    document.getElementById("id_refractive_index").value = source_tr.children["refractive-index-" + num].innerText;
+    document.getElementById("id_picture_size").value = source_tr.children["picture-size-" + num].innerText;
+    document.getElementById("id_incident_light_intensity").value = source_tr.children["incident-light-intensive-" + num].innerText;
+    document.getElementById("id_N").value = source_tr.children["n-" + num].innerText;
 }
