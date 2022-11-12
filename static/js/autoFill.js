7f1758f894c1eb8e1b9f628b09ac0e4ddccf780b
Yury Katasonov <katasonov-yury@yandex.ru>
2022-11-12 12:55:42 +0300

    add AutoFillM.js script for parse

--- static/js/autoFill.js
+++ static/js/autoFill.js
@@ -9,3 +9,15 @@ function autoFill(num) {
     document.getElementById("id_incident_light_intensity").value = document.getElementById("incident-light-intensive-" + num).innerText;
     document.getElementById("id_N").value = document.getElementById("n-" + num).innerText;
 }
+
+function autoFillM(num) {
+    document.getElementById("id_wavelength").value = document.getElementById("wavelength-" + num).innerText;
+    document.getElementById("id_z1").value = document.getElementById("z1-" + num).innerText;
+    document.getElementById("id_z2").value = document.getElementById("z2-" + num).innerText;
+    document.getElementById("id_Rbs").value = document.getElementById("Rbs-" + num).innerText;
+    document.getElementById("id_tx").value = document.getElementById("tx-" + num).innerText;
+    document.getElementById("id_ty").value = document.getElementById("ty-" + num).innerText;
+    document.getElementById("id_f").value = document.getElementById("f-" + num).innerText;
+    document.getElementById("id_size").value = document.getElementById("size-" + num).innerText;
+    document.getElementById("id_N").value = document.getElementById("n-" + num).innerText;
+}
