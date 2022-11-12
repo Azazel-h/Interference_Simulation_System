2ce87ff96ee54e57dc3dce003c46bc34d514380b
Sers <serspgl@gmail.com>
2022-11-12 20:33:04 +0300

    Add loading animation for graph update

--- static/js/updateWithoutReload.js
+++ static/js/updateWithoutReload.js
@@ -14,6 +14,13 @@ function updateGraph(is_authorized, csrftoken) {
             "X-CSRFToken": csrftoken
         },
         data: request_data,
+        beforeSend: function() {
+            $("#graph").html(
+                "<svg class=\"spinner\" viewBox=\"0 0 50 50\">" +
+                "  <circle class=\"path\" cx=\"25\" cy=\"25\" r=\"20\" fill=\"none\" stroke-width=\"5\"></circle>" +
+                "</svg>"
+            );
+        },
         success: function(response) {
             $("#graph").html(response);
         }
