9e94d7fde7f185eccd5f8482b99c9eb5b4fedfb1
Sersoid <36604673+Sersoid@users.noreply.github.com>
2022-11-13 23:06:54 +0300

    Make sliders change values after autoFill.js

--- static/js/autoFill.js
+++ static/js/autoFill.js
@@ -1,27 +1,44 @@
 function autoFill(num, source) {
     const source_tr = document.getElementById(source).children[1].children[num - 1];
+    const id_and_value = [
+        ["id_wave_length", "wave-length-" + num],
+        ["id_glasses_distance", "glasses-dist-" + num],
+        ["id_focal_distance", "focal-distance-" + num],
+        ["id_stroke_difference", "stroke-difference-" + num],
+        ["id_reflectivity", "reflectivity-" + num],
+        ["id_refractive_index", "refractive-index-" + num],
+        ["id_picture_size", "picture-size-" + num],
+        ["id_incident_light_intensity", "incident-light-intensive-" + num],
+        ["id_N", "n-" + num]
+    ]
 
-    document.getElementById("id_wave_length").value = source_tr.children["wave-length-" + num].innerText;
-    document.getElementById("id_glasses_distance").value = source_tr.children["glasses-dist-" + num].innerText;
-    document.getElementById("id_focal_distance").value = source_tr.children["focal-distance-" + num].innerText;
-    document.getElementById("id_stroke_difference").value = source_tr.children["stroke-difference-" + num].innerText;
-    document.getElementById("id_reflectivity").value = source_tr.children["reflectivity-" + num].innerText;
-    document.getElementById("id_refractive_index").value = source_tr.children["refractive-index-" + num].innerText;
-    document.getElementById("id_picture_size").value = source_tr.children["picture-size-" + num].innerText;
-    document.getElementById("id_incident_light_intensity").value = source_tr.children["incident-light-intensive-" + num].innerText;
-    document.getElementById("id_N").value = source_tr.children["n-" + num].innerText;
+    fillValues(source_tr, id_and_value);
 }
 
 function autoFillM(num, source) {
     const source_tr = document.getElementById(source).children[1].children[num - 1];
+    const id_and_value = [
+        ["id_wavelength", "wavelength-" + num],
+        ["id_z1", "z1-" + num],
+        ["id_z2", "z2-" + num],
+        ["id_Rbs", "Rbs-" + num],
+        ["id_tx", "tx-" + num],
+        ["id_ty", "ty-" + num],
+        ["id_f", "f-" + num],
+        ["id_size", "size-" + num],
+        ["id_N", "n-" + num]
+    ]
 
-    document.getElementById("id_wavelength").value = source_tr.children["wavelength-" + num].innerText;
-    document.getElementById("id_z1").value = source_tr.children["z1-" + num].innerText;
-    document.getElementById("id_z2").value = source_tr.children["z2-" + num].innerText;
-    document.getElementById("id_Rbs").value = source_tr.children["Rbs-" + num].innerText;
-    document.getElementById("id_tx").value = source_tr.children["tx-" + num].innerText;
-    document.getElementById("id_ty").value = source_tr.children["ty-" + num].innerText;
-    document.getElementById("id_f").value = source_tr.children["f-" + num].innerText;
-    document.getElementById("id_size").value = source_tr.children["size-" + num].innerText;
-    document.getElementById("id_N").value = source_tr.children["n-" + num].innerText;
+    fillValues(source_tr, id_and_value);
+}
+
+function fillValues(source_tr, id_and_value) {
+    for (let i in id_and_value) {
+        document.getElementById(id_and_value[i][0]).value = source_tr.children[id_and_value[i][1]].innerText;
+
+        let next_elem = document.getElementById(id_and_value[i][0]).nextElementSibling
+        if (next_elem && next_elem.hasAttribute("id") && next_elem.getAttribute("id") === id_and_value[i][0]) {
+            next_elem.value = source_tr.children[id_and_value[i][1]].innerText;
+        }
+    }
 }
