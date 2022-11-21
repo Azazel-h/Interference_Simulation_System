af2ec8fd60aab3884312c5479c001dfc3a906499
Sersoid <36604673+Sersoid@users.noreply.github.com>
2022-11-21 20:03:51 +0300

    Add TTL parameter for database

--- interferometers/settings.py
+++ interferometers/settings.py
@@ -81,9 +81,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        'CONN_MAX_AGE': 600,
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
