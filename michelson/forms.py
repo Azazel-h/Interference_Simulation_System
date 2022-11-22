f7b44e4b196279799c34fe4af10cc4f4dcf87962
Sersoid <36604673+Sersoid@users.noreply.github.com>
2022-11-22 20:58:19 +0300

    Add LaTeX unit display for michelson

--- michelson/forms.py
+++ michelson/forms.py
@@ -3,16 +3,16 @@ from django import forms
 
 class GraphForm(forms.Form):
     wavelength = forms.FloatField(
-        label='Длина волны гелий-неонового лазера [нм]',
+        label='Длина волны гелий-неонового лазера [\\(нм\\)]',
         min_value=200,
         max_value=800,
         initial=632.8)
     z1 = forms.FloatField(
-        label='Длина первого плеча [см]',
+        label='Длина первого плеча [\\(см\\)]',
         min_value=0,
         initial=8)
     z2 = forms.FloatField(
-        label='Длина второго плеча [см]',
+        label='Длина второго плеча [\\(см\\)]',
         min_value=0,
         initial=7)
     Rbs = forms.FloatField(
@@ -20,19 +20,19 @@ class GraphForm(forms.Form):
         min_value=0,
         initial=0.5)
     tx = forms.FloatField(
-        label='Наклон зеркала по X [мрад]',
+        label='Наклон зеркала по X [\\(мрад\\)]',
         min_value=0,
         initial=1)
     ty = forms.FloatField(
-        label='Наклон зеркала по Y [мрад]',
+        label='Наклон зеркала по Y [\\(мрад\\)]',
         min_value=0,
         initial=0)
     f = forms.FloatField(
-        label='Фокусное расстояние положительной линзы [см]',
+        label='Фокусное расстояние положительной линзы [\\(см\\)]',
         min_value=0,
         initial=50)
     size = forms.FloatField(
-        label='Размер рисунка [мм]',
+        label='Размер рисунка [\\(мм\\)]',
         min_value=0,
         initial=10)
     N = forms.IntegerField(
