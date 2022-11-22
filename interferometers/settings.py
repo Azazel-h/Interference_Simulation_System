c490699f1dd737bd31dac8a15c1466dcfb4fc047
Sersoid <36604673+Sersoid@users.noreply.github.com>
2022-11-22 21:48:30 +0300

    Remove unused middlewares

--- interferometers/settings.py
+++ interferometers/settings.py
@@ -36,13 +36,11 @@ INSTALLED_APPS = [
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django_cas_ng.middleware.CASMiddleware'
 ]
 
