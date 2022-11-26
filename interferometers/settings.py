6c254f5ddfdb0ef9bdfee709f0d14d2642240af7
Sers <serspgl@gmail.com>
2022-11-26 14:52:56 +0300

    Return SecurityMiddleware

--- interferometers/settings.py
+++ interferometers/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
