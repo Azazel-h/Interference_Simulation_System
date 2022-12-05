02fdbd93c45623569d72d1d792565a0c8fa09b10
Kirill <missl.wipiece@gmail.com>
2022-12-05 10:10:03 +0300

    changes requested by sersoid

--- fabry_perot/tests.py
+++ fabry_perot/tests.py
@@ -3,14 +3,14 @@ from django.urls import reverse
 
 
 class TestPage(TestCase):
-    def assert_generates(self, data, code, content=None):
+    def assert_updates_graph(self, data, code, content=None):
         url = reverse('index') + 'update-graph/'
         response = self.client.post(url, data)
         self.assertEqual(response.status_code, code)
         if content:
             self.assertEqual(response.content, content)
 
-    def assert_saves(self, data, code, content=None):
+    def assert_saves_preset(self, data, code, content=None):
         data["preset_operation"] = "save_preset"
         url = reverse('index') + 'update-preset/'
         response = self.client.post(url, data)
@@ -18,7 +18,7 @@ class TestPage(TestCase):
         if content:
             self.assertContains(response, content)
 
-    def assert_deletes(self, preset_id, code, content=None):
+    def assert_deletes_preset(self, preset_id, code, content=None):
         data = {
             "delete_preset": preset_id,
             "preset_operation": "delete_preset"
@@ -62,58 +62,57 @@ class TestPage(TestCase):
 
     def test_interferometer_generation(self):
         request_data = self.get_data()
-
-        self.assert_generates(request_data, 200)
+        self.assert_updates_graph(request_data, 200)
 
     def test_interferometer_generation_with_wrong_wavelength(self):
         request_data = self.get_data(wave_length=1000)
-        self.assert_generates(request_data, 200, b'None')
+        self.assert_updates_graph(request_data, 200, b'None')
 
         request_data = self.get_data(wave_length=0)
-        self.assert_generates(request_data, 200, b'None')
+        self.assert_updates_graph(request_data, 200, b'None')
 
     def test_interferometer_generation_with_wrong_glasses_distance(self):
         request_data = self.get_data(glasses_distance=-1)
-        self.assert_generates(request_data, 200, b'None')
+        self.assert_updates_graph(request_data, 200, b'None')
 
     def test_interferometer_generation_with_wrong_focal_distance(self):
         request_data = self.get_data(focal_distance=-1)
-        self.assert_generates(request_data, 200, b'None')
+        self.assert_updates_graph(request_data, 200, b'None')
 
     def test_interferometer_generation_with_wrong_stroke_difference(self):
         request_data = self.get_data(stroke_difference=-1)
-        self.assert_generates(request_data, 200, b'None')
+        self.assert_updates_graph(request_data, 200, b'None')
 
     def test_interferometer_generation_with_wrong_picture_size(self):
         request_data = self.get_data(picture_size=-1)
-        self.assert_generates(request_data, 200, b'None')
+        self.assert_updates_graph(request_data, 200, b'None')
 
     def test_interferometer_generation_with_wrong_N(self):
         request_data = self.get_data(N=-1)
-        self.assert_generates(request_data, 200, b'None')
+        self.assert_updates_graph(request_data, 200, b'None')
 
         # N more than 4000
         request_data = self.get_data(N=4001)
-        self.assert_generates(request_data, 200, b'None')
+        self.assert_updates_graph(request_data, 200, b'None')
 
     def test_interferometer_generation_with_wrong_reflectivity(self):
         request_data = self.get_data(reflectivity=-1)
-        self.assert_generates(request_data, 200, b'None')
+        self.assert_updates_graph(request_data, 200, b'None')
 
         request_data = self.get_data(reflectivity=1.5)
-        self.assert_generates(request_data, 200, b'None')
+        self.assert_updates_graph(request_data, 200, b'None')
 
     def test_interferometer_generation_with_wrong_refractive_index(self):
         request_data = self.get_data(refractive_index=-1)
-        self.assert_generates(request_data, 200, b'None')
+        self.assert_updates_graph(request_data, 200, b'None')
 
     def test_save_preset_not_saves_without_auth(self):
         request_data = self.get_data()
-        self.assert_saves(self.get_data(), 200, 'Вы пока не сохранили ни одного набора данных.'.encode('utf-8'))
-        self.assert_saves(self.get_data(), 200, 'Вы пока не сохранили ни одного набора данных.'.encode('utf-8'))
+        self.assert_saves_preset(self.get_data(), 200, 'Вы пока не сохранили ни одного набора данных.'.encode('utf-8'))
+        self.assert_saves_preset(self.get_data(), 200, 'Вы пока не сохранили ни одного набора данных.'.encode('utf-8'))
 
     def test_delete_empty_returns_empty(self):
-        self.assert_deletes(0, 200, 'Вы пока не сохранили ни одного набора данных.'.encode('utf-8'))
+        self.assert_deletes_preset(0, 200, 'Вы пока не сохранили ни одного набора данных.'.encode('utf-8'))
 
     def test_update_history_returns_empty(self):
         self.assert_update_history(self.get_data(), 200, 'Вы пока не сделали ни одного запроса.'.encode('utf-8'))
