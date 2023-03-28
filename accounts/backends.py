4acf4b0549a882b47d1ba10ac2009d4e00e976bb
Sersoid <serspgl@gmail.com>
2023-03-28 22:46:17 +0300

    Remove unused code from CAS/LDAP auth backend

--- accounts/backends.py
+++ accounts/backends.py
@@ -18,27 +18,10 @@ class AuthBackend(CASBackend):
         if attributes and request:
             request.session['attributes'] = attributes
 
-        if settings.CAS_USERNAME_ATTRIBUTE != 'cas:user' and settings.CAS_VERSION != 'CAS_2_SAML_1_0':
-            if attributes:
-                username = attributes.get(settings.CAS_USERNAME_ATTRIBUTE)
-            else:
-                return None
-
         if not username:
             return None
 
         username = self.clean_username(username)
-
-        if attributes:
-            reject = self.bad_attributes_reject(request, username, attributes)
-            if reject:
-                return None
-
-            for cas_attr_name, req_attr_name in settings.CAS_RENAME_ATTRIBUTES.items():
-                if cas_attr_name in attributes and cas_attr_name is not req_attr_name:
-                    attributes[req_attr_name] = attributes[cas_attr_name]
-                    attributes.pop(cas_attr_name)
-
         user_model = get_user_model()
         user_ldap_info = None
         is_staff = True
@@ -52,6 +35,7 @@ class AuthBackend(CASBackend):
 
                 if data:
                     user_ldap_info = data[0][1]
+                    user_ldap_info['is_staff'] = is_staff
 
                 is_staff = False
 
@@ -62,46 +46,20 @@ class AuthBackend(CASBackend):
         user_kwargs = {
             'uid': user_ldap_info['uid'][0].decode(),
             'mail': user_ldap_info['mail'][0].decode(),
-            'is_staff': is_staff,
+            'is_staff': user_ldap_info['is_staff'],
             'first_name': full_name[1],
             'last_name': full_name[0],
             'patronymic': full_name[2] if len(full_name) == 3 else '',
             'title': user_ldap_info['ou'][0].decode(),
         }
 
-        if settings.CAS_CREATE_USER_WITH_ID:
-            user_kwargs['id'] = self.get_user_id(attributes)
-
         user, created = user_model._default_manager.update_or_create(uid=username, defaults=user_kwargs)
+
         if created:
             user = self.configure_user(user)
 
         if not self.user_can_authenticate(user):
             return None
-
-        if pgtiou and settings.CAS_PROXY_CALLBACK and request:
-            request.session['pgtiou'] = pgtiou
-
-        if settings.CAS_APPLY_ATTRIBUTES_TO_USER and attributes:
-            user_model_fields = user_model._meta.fields
-            for field in user_model_fields:
-                if not field.null:
-                    try:
-                        if attributes[field.name] is None:
-                            attributes[field.name] = ''
-                    except KeyError:
-                        continue
-                if field.get_internal_type() == 'BooleanField':
-                    try:
-                        boolean_value = attributes[field.name] == 'True'
-                        attributes[field.name] = boolean_value
-                    except KeyError:
-                        continue
-
-            user.__dict__.update(attributes)
-
-            if settings.CAS_CREATE_USER:
-                user.save()
 
         cas_user_authenticated.send(
             sender=self,
