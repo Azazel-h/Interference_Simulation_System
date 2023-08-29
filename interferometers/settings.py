a5c77f0663ddefe0d3edead5e3f423efcb85f9dd
Sersoid <serspgl@gmail.com>
2023-08-29 10:55:02 +0300

    Update history table pagination

--- interferometers/settings.py
+++ interferometers/settings.py
@@ -87,6 +87,9 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
+            'libraries': {
+                'paginator_tags': 'templates.tags.paginator_tags',
+            }
         },
     },
 ]
