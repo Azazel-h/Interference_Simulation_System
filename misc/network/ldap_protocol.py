f71567569b22f52976ff0fda8f1f036a7f5ae1d5
Yasuhiro <yasuhiro.corp@gmail.com>
2023-09-20 21:39:23 +0300

    Can I do it without certs?

--- misc/network/ldap_protocol.py
+++ misc/network/ldap_protocol.py
@@ -29,14 +29,13 @@ class LDAPConnection:
 
         try:
             ldap.set_option(ldap.OPT_X_TLS_REQUIRE_CERT, ldap.OPT_X_TLS_NEVER)
-            ldap.set_option(ldap.OPT_X_TLS_CACERTFILE, '/ca-certificates.crt')
             self.connection = ldap.initialize(self.server_uri)
 
             self.connection.set_option(ldap.OPT_REFERRALS, 0)
             self.connection.set_option(ldap.OPT_PROTOCOL_VERSION, 3)
             self.connection.set_option(ldap.OPT_X_TLS, ldap.OPT_X_TLS_DEMAND)
             self.connection.set_option(ldap.OPT_X_TLS_DEMAND, True)
-
+            self.connection.set_option(ldap.OPT_DEBUG_LEVEL, 255)
             self.connection.start_tls_s()
             self.connection.simple_bind_s(self.bind_dn, self.bind_password)
         except (ldap.LDAPError, ldap.SERVER_DOWN) as error:
